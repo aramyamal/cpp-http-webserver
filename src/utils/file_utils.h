5025b083e0884b214370327477ba5fa10fbda8f3
aramyamal <aram@aramjamal.com>
2025-02-21 14:43:28 +0100

    feat(serving): only serve files from public dir

--- src/utils/file_utils.h
+++ src/utils/file_utils.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <optional>
 #include <string>
+#include <vector>
 namespace web {
 
 std::optional<std::string> read_file(std::string_view path);
 
+std::vector<std::string> index_public();
+
+std::optional<std::string>
+read_file_from_dir(std::string_view rel_path, std::string_view dir_name,
+                   std::vector<std::string> dir_index);
 } // namespace web
